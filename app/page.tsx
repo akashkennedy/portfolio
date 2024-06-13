eeba2846f66e1ef8cfff6b040856baa729d59300
Akash Kennedy <akashkennedy1@gmail.com>
2024-06-13 08:31:42 +0530

    Create Social Links Sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,40 @@
-"use client"
+"use client";
 
 import TypeIt from "typeit-react";
 import Button from "@/components/Button";
-
-
+import SocialSidebar from "@/components/SocialSidebar";
 
 const Home = () => {
-
   return (
     <main className="mx-24">
-      <div className="min-h-screen flex items-start justify-center flex-col max-w-4xl gap-5">
-        <h2 className="text-2xl">Hello I'm <span id="heading" className="text-secondary"><TypeIt>Akash Kennedy</TypeIt></span></h2>
-        <div className="space-y-2">
-        <h1>A Front End <span className="text-secondary">Developer</span></h1>
-        <p>A Self Taught Front End developer with Great Experience with Latest Web Technologies.Specialized in React Js and Tailwind CSS with writing Robust and Maintainable Code using Typescript.</p>
+      <div className="min-h-screen flex items-start justify-center flex-col max-w-3xl gap-5">
+        <h2 className="text-2xl">
+          Hello I'm{" "}
+          <span id="heading" className="text-secondary">
+            <TypeIt>Akash Kennedy</TypeIt>
+          </span>
+        </h2>
+        <div className="space-y-3">
+          <h1>
+            A Front End <span className="text-secondary">Developer</span>
+          </h1>
+          <p className="text-base">
+            A Self Taught Front End developer with Great Experience with Latest
+            Web Technologies.Specialized in React Js and Tailwind CSS with
+            writing Robust and Maintainable Code using Typescript.
+          </p>
         </div>
-      <div className="space-x-3">
+        <div className="space-x-3">
           <Button label="Resume" />
-          <Button label="Contact" className="border bg-white bg-opacity-10 backdrop-blur-3xl" />
+          <Button
+            label="Contact"
+            className="border bg-white bg-opacity-10 backdrop-blur-3xl"
+          />
         </div>
+        <SocialSidebar />
       </div>
     </main>
-  )
-}
+  );
+};
 
 export default Home;
---
+++ components/SocialSidebar.tsx
@@ -0,0 +1,25 @@
+import { Github, Linkedin, Twitter } from "lucide-react";
+
+const SocialSidebar = () => {
+  return (
+    <aside className="absolute border bg-white bg-opacity-10 backdrop-blur-3xl min-w-20 min-h-96 right-0 rounded-l-3xl flex flex-col items-center justify-center gap-12">
+      <div className="cursor-pointer">
+        <a href="https://www.github.com/akashkennedy" target="_blank">
+          <Github />
+        </a>
+      </div>
+      <div>
+        <a href="https://www.linkedin.com/in/akashkennedy" target="_blank">
+          <Linkedin />
+        </a>
+      </div>
+      <div>
+        <a href="https://www.twitter.com/akash_kennedy1" target="_blank">
+          <Twitter />
+        </a>
+      </div>
+    </aside>
+  );
+};
+
+export default SocialSidebar;
