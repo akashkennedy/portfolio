6c0250b076a93fbb6bf50d00b65bb3121da7399a
Akash Kennedy <akashkennedy1@gmail.com>
2024-06-13 19:28:57 +0530

    Update Tablet and Mobile Layouts

--- app/page.tsx
+++ app/page.tsx
@@ -11,9 +11,9 @@ import { Progress } from "scroll-up-react";
 
 const Home = () => {
   return (
-    <main className="mx-24">
+    <main className="mx-5 md:mx-10 lg:mx-24">
       <Progress color2="#7127BA" size="3px" />
-      <div className="min-h-screen flex items-start justify-center flex-col max-w-3xl gap-5">
+      <div className="my-20 md:my-28 lg:min-h-screen lg:my-0 flex items-start justify-center flex-col max-w-3xl gap-5">
         <h2 className="text-2xl">
           Hello I'm{" "}
           <span id="heading" className="text-secondary">
@@ -26,24 +26,24 @@ const Home = () => {
             height={200}
             width={350}
             alt="Ellipse"
-            className="absolute right-3"
+            className="absolute top-11 md:right-20 md:top-1 lg:right-28 lg:top-2"
           />
-          <h1>
+          <h1 className="text-5xl my-3 lg:block">
             A Front End <span className="text-secondary">Developer</span>
           </h1>
-          <p className="text-base">
+          <p className="py-3 leading-loose">
             A Self Taught Front End developer with Great Experience in Latest
             Web Technologies.Specialized in React Js and Tailwind CSS with
             writing Robust and Maintainable Code using Typescript.
           </p>
         </div>
-        <div className="space-x-3">
+        <div className="space-y-5 w-full lg:space-x-3 lg:w-fit">
           <a href="/Akash-Kennedy.pdf" download={true} target="_blank">
-            <Button label="Resume" />
+            <Button label="Resume" className="w-full lg:w-48" />
           </a>
           <Button
             label="Contact"
-            className="border bg-white bg-opacity-10 backdrop-blur-3xl"
+            className="border bg-white bg-opacity-10 backdrop-blur-3xl w-full lg:w-48"
           />
         </div>
         <SocialSidebar />
