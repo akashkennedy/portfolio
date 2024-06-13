d6e581eac5ae88b6c47133e0230d50b03cc4e9b3
Akash Kennedy <akashkennedy1@gmail.com>
2024-06-13 07:58:50 +0530

    Update Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
 "use client"
 
 import TypeIt from "typeit-react";
+import Button from "@/components/Button";
+
+
 
 const Home = () => {
+
   return (
     <main className="mx-24">
       <div className="min-h-screen flex items-start justify-center flex-col max-w-4xl gap-5">
-        <h2 className="text-2xl">Hello I'm <span className="text-secondary"><TypeIt>Akash Kennedy</TypeIt></span></h2>
+        <h2 className="text-2xl">Hello I'm <span id="heading" className="text-secondary"><TypeIt>Akash Kennedy</TypeIt></span></h2>
         <div className="space-y-2">
         <h1>A Front End <span className="text-secondary">Developer</span></h1>
         <p>A Self Taught Front End developer with Great Experience with Latest Web Technologies.Specialized in React Js and Tailwind CSS with writing Robust and Maintainable Code using Typescript.</p>
+        </div>
+      <div className="space-x-3">
+          <Button label="Resume" />
+          <Button label="Contact" className="border bg-white bg-opacity-10 backdrop-blur-3xl" />
         </div>
       </div>
     </main>
---
+++ components/Button.tsx
@@ -0,0 +1,17 @@
+
+
+interface ButtonProps {
+    label: string,
+    className?: string
+}
+
+const Button = ({label,className}: ButtonProps) => {
+    return (
+    <button className={`w-48 bg-secondary h-[3.5rem] rounded-lg font-semibold text-xl ${className}`}>
+       {label} 
+    </button>
+    )
+
+}
+
+export default Button;
