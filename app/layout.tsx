d82fd7601d152171b36718ad9ca4fdffc40f9ba4
Akash Kennedy <akashkennedy1@gmail.com>
2024-06-14 12:17:29 +0530

    Add Vercel Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Preahvihear } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const preahvihear = Preahvihear({
   weight: "400",
